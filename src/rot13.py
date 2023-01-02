966e54782262070800f55583faf7e90f9f4db796
Kartikey Subramanium <kartikey@tutanota.com>
2023-01-03 03:09:29 +0530

    add rot13 utility

---
+++ src/rot13.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+
+
+import sys
+
+
+def arg_help(*args, **kwargs):
+    print(f"Usage: {args[0]} [TEXT]")
+    sys.exit(0)
+
+def rot13(words: str) -> str:
+    out = ""
+    for char in words:
+        if (char.isalpha()):
+            if (((ord("z") - ord(char)) > 13) ^ ((ord("Z") - ord(char)) > 13)):
+                out += chr(ord(char) + 13)
+            else:
+                out += chr(ord(char) - 13)
+        else:
+            out += char
+    return out
+
+
+def main(*args, **kwargs):
+    if (("-h" in args) or ("--help" in args)):
+        arg_help(*args, *kwargs)
+    if (len(args) > 1):
+        words = ""
+        for word in args[1:]:
+            words += word + " "
+        words.strip()
+    else:
+        words = sys.stdin.read().strip()
+
+    print(rot13(words))
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main(*sys.argv)
